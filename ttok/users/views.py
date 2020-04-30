84fe63ddc2a246fdbd0b5e75891084c8416ccb31
RenatoBrittoAraujo <renatomwbbritto@gmail.com>
2020-04-29 22:02:49 -0300

    Remove unecessary code

--- ttok/users/views.py
+++ ttok/users/views.py
@@ -26,55 +26,9 @@ class UserProfile(generics.RetrieveAPIView):
 
     lookup_field = 'username'
 
-    def get_object(self):
-        queryset = self.filter_queryset(self.get_queryset())
-
-        # Perform the lookup filtering.
-        lookup_url_kwarg = self.lookup_url_kwarg or self.lookup_field
-        print('lookup_url_kwarg:', lookup_url_kwarg)
-        print('lookup_field:', self.lookup_field)
-        print('self.kwargs', self.kwargs)
-
-        assert lookup_url_kwarg in self.kwargs, (
-            'Expected view %s to be called with a URL keyword argument '
-            'named "%s". Fix your URL conf, or set the `.lookup_field` '
-            'attribute on the view correctly.' %
-            (self.__class__.__name__, lookup_url_kwarg)
-        )
-
-        filter_kwargs = {self.lookup_field: self.kwargs[lookup_url_kwarg]}
-        print(filter_kwargs)
-        obj = get_object_or_404(queryset, **filter_kwargs)
-
-        # May raise a permission denied
-        self.check_object_permissions(self.request, obj)
-
-        return obj
-
 class UserUpdateProfile(generics.UpdateAPIView):
     permission_classes = (IsOwner, )
     serializer_class = UserProfileUpdateSerializer
     queryset = User.objects.all()
 
     lookup_field = 'username'
-
-    def get_object(self):
-        queryset = self.filter_queryset(self.get_queryset())
-
-        # Perform the lookup filtering.
-        lookup_url_kwarg = self.lookup_url_kwarg or self.lookup_field
-
-        assert lookup_url_kwarg in self.kwargs, (
-            'Expected view %s to be called with a URL keyword argument '
-            'named "%s". Fix your URL conf, or set the `.lookup_field` '
-            'attribute on the view correctly.' %
-            (self.__class__.__name__, lookup_url_kwarg)
-        )
-
-        filter_kwargs = {self.lookup_field: self.kwargs[lookup_url_kwarg]}
-        obj = get_object_or_404(queryset, **filter_kwargs)
-
-        # May raise a permission denied
-        self.check_object_permissions(self.request, obj)
-
-        return obj
